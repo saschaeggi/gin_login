878bfca0007b6cef588770c5c89a1954e83c32e0
Sascha Eggenberger <hello@saschaeggenberger.com>
2022-11-28 20:20:04 +0100

    Image selection, imgoptim

--- dist/js/login.js
+++ dist/js/login.js
@@ -7,9 +7,9 @@
     }
   }, Drupal.ginLogin = {
     randomWallpaper: () => {
-      const path = drupalSettings.gin_login.path + "/images/wallpapers/", wallpapers = [ "eberhard-grossgasteiger-f4OFym_1JsM-unsplash.jpg", "eberhard-grossgasteiger-v5qZ8VnPamg-unsplash.jpg", "eberhard-grossgasteiger-KVKYuYjKPnk-unsplash.jpg", "eberhard-grossgasteiger-7JzgB1NpSW4-unsplash.jpg", "claudio-schwarz-XmXQ5Ek60pk-unsplash.jpg", "claudio-schwarz-mYY3NNP47pU-unsplash.jpg", "claudio-schwarz-WuhakAl5JyY-unsplash.jpg", "vlado-paunovic-gZegJ2zI0_I-unsplash.jpg", "micha-sager-lsDJyu_uJhw-unsplash.jpg" ], wallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
+      const path = drupalSettings.gin_login.path + "/images/wallpapers/", wallpapers = [ "eberhard-grossgasteiger-uE1fGoWWHsY-unsplash.jpg", "eberhard-grossgasteiger-v5qZ8VnPamg-unsplash.jpg", "eberhard-grossgasteiger-ULC4CL2MGK8-unsplash.jpg", "eberhard-grossgasteiger-mt1sZs4QBcw-unsplash.jpg", "claudio-schwarz-mYY3NNP47pU-unsplash.jpg", "claudio-schwarz-XmXQ5Ek60pk-unsplash.jpg", "micha-sager-lsDJyu_uJhw-unsplash.jpg" ], wallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
       let image = new Image;
-      image.src = path + wallpaper, document.querySelector(".user-form-page__wallpaper").appendChild(image);
+      image.src = path + wallpaper, document.querySelector(".gin-login .user-form-page__wallpaper").appendChild(image);
     }
   };
 })(Drupal, drupalSettings, once);
--- src/js/login.js
+++ src/js/login.js
@@ -12,21 +12,19 @@
     randomWallpaper: () => {
       const path = drupalSettings.gin_login.path + '/images/wallpapers/';
       const wallpapers = [
-        'eberhard-grossgasteiger-f4OFym_1JsM-unsplash.jpg',
+        'eberhard-grossgasteiger-uE1fGoWWHsY-unsplash.jpg',
         'eberhard-grossgasteiger-v5qZ8VnPamg-unsplash.jpg',
-        'eberhard-grossgasteiger-KVKYuYjKPnk-unsplash.jpg',
-        'eberhard-grossgasteiger-7JzgB1NpSW4-unsplash.jpg',
-        'claudio-schwarz-XmXQ5Ek60pk-unsplash.jpg',
+        'eberhard-grossgasteiger-ULC4CL2MGK8-unsplash.jpg',
+        'eberhard-grossgasteiger-mt1sZs4QBcw-unsplash.jpg',
         'claudio-schwarz-mYY3NNP47pU-unsplash.jpg',
-        'claudio-schwarz-WuhakAl5JyY-unsplash.jpg',
-        'vlado-paunovic-gZegJ2zI0_I-unsplash.jpg',
+        'claudio-schwarz-XmXQ5Ek60pk-unsplash.jpg',
         'micha-sager-lsDJyu_uJhw-unsplash.jpg',
       ];
 
       const wallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
       let image = new Image();
       image.src = path+wallpaper;
-      document.querySelector('.user-form-page__wallpaper').appendChild(image);
+      document.querySelector('.gin-login .user-form-page__wallpaper').appendChild(image);
     },
 
   };
